5073ebca2b56fc699e5f99a3ab29b7f6f91435d9
jixinqi <577591592@qq.com>
2019-08-19 14:23:59 +0800

    初始化项目-页面分析-新建登录和主页

--- src/router.js
+++ src/router.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+import Home from './views/home'
+import login from './views/login'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/',
+      name: 'login',
+      component: login
     }
     // {
     //   path: '/about',
---
+++ src/views/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.login {
+  width: 100%;
+  height: 100vh;
+  background: url('../../assets/login_bg.jpg');
+  background-size: cover;
+}
+
+</style>
