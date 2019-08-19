22fdf2f5004a302edd0a586d758bea78fee291a5
jixinqi <577591592@qq.com>
2019-08-19 10:29:40 +0800

    解决冲突,初始化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import Ele from 'element-ui'
+import '../node_modules/element-ui/lib/theme-chalk/index.css'
+Vue.use(Ele)
 Vue.config.productionTip = false
 
 new Vue({
